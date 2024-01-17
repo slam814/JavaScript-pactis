9a2e37645285e5673a8aa1d27c9f97f0e15efb0b
slam814 <abdussalam6111997@gmail.com>
2024-01-17 14:50:21 +0600

    i added some file

---
+++ 22modul/exam.js
@@ -0,0 +1,19 @@
+// const nayoks=['“Bangla Bhai”','”English Bhai”','”Korean Bhai ”','”Turkish Bhai ”'];
+// for(const nayok of nayoks) { 
+//     console.log(nayok);
+// };
+
+// let sum= 0; 
+
+// for( let i = 0; i<=3; i++){ 
+
+// sum = sum + i; 
+
+// }
+// console.log(sum);
+
+
+let arr = [1, 3, 5];
+ arr[arr.length-1] = 5;
+
+console.log(arr);
